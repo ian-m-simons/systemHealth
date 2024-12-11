1e08e464e187786ea8da768bee00f5feb6975287
Ian Simons <iansimons93@gmail.com>
2024-12-11 14:35:26 -0500

    adding disk free

--- systemHealth.py
+++ systemHealth.py
@@ -1,8 +1,12 @@
 import psutil
+import shutil
 
 def main():
-    print(psutil.cpu_percent())
-    print(psutil.virtual_memory().available * 100 / psutil.virtual_memory().total)
+    print(psutil.cpu_percent(), "% CPU usage")
+    print(psutil.virtual_memory().available * 100 / psutil.virtual_memory().total, "% available RAM")
+    total, used, free = shutil.disk_usage("/")
+    print((free / total) * 100, "% free disk Space")
+
 
 if __name__ == "__main__":
     main()
