cf2054175405f4099bc5ba4594705c820bd52c0f
Ian Simons <iansimons93@gmail.com>
2024-12-11 14:47:37 -0500

    including uptime

--- systemHealth.py
+++ systemHealth.py
@@ -1,7 +1,18 @@
 import psutil
 import shutil
+import datetime
+import time
+
+def get_uptime():
+    boot_time = psutil.boot_time()
+    now = time.time()
+    uptime_seconds = int(now - boot_time)
+    uptime_formatted = str(datetime.timedelta(seconds=uptime_seconds))
+    return uptime_formatted
 
 def main():
+    uptime = get_uptime()
+    print("current uptime: "+uptime)
     print(psutil.cpu_percent(), "% CPU usage")
     print(psutil.virtual_memory().available * 100 / psutil.virtual_memory().total, "% memory available")
     total, used, free = shutil.disk_usage("/")
