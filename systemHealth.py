3d357533755d654c1432813bab394b588ed59bc2
Ian Simons <iansimons93@gmail.com>
2024-12-11 14:37:06 -0500

    updating output

--- systemHealth.py
+++ systemHealth.py
@@ -3,7 +3,7 @@ import shutil
 
 def main():
     print(psutil.cpu_percent(), "% CPU usage")
-    print(psutil.virtual_memory().available * 100 / psutil.virtual_memory().total, "% available RAM")
+    print(psutil.virtual_memory().available * 100 / psutil.virtual_memory().total, "% memory available")
     total, used, free = shutil.disk_usage("/")
     print((free / total) * 100, "% free disk Space")
 
