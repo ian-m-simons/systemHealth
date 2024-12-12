5e8d5a49129c04a958f49d65f0f65925fa68a93b
Ian Simons <iansimons93@gmail.com>
2024-12-12 11:11:29 -0500

    adding menu and integer input validation

--- systemHealth.py
+++ systemHealth.py
@@ -3,6 +3,17 @@ import shutil
 import datetime
 import time
 
+def inputInt(prompt):
+    success = False
+    while not success:
+        value = input(prompt)
+        try:
+            value = int(value)
+            success = True
+        except:
+            print("[ERROR] Input must be integer")
+    return value
+
 def get_uptime():
     boot_time = psutil.boot_time()
     now = time.time()
@@ -28,28 +39,35 @@ def getDiskSpace():
     freeDisk = (free/total)*100
     return freeDisk
 
+def check():
+    CPU = getCPUusage()
+    memory = getMemory()
+    freeDisk = getDiskSpace()
+    CPUHealth = True
+    memoryHealth = True
+    diskHealth = True
+    if CPU > 50:
+        print("\a[WARNING] sustained higher CPU usage\nCPU has averaged " + str(CPU) + "% for 5 minutes")
+        CPUHealth = False
+    if memory < 10:
+        print("\a[WARNING] RAM usage is at " + str(100-memory) + "%")
+        memoryHealth = False
+    if freeDisk < 10:
+        print("\a[WARNING] Disk almost full, only " + str(freeDisk) + "% remaning")
+        diskHealth = False
+    if CPUHealth and memoryHealth and diskHealth:
+        print("system is healthy")
+
 def main():
-    
+    print("Welcome!")
     while True:
-        uptime = get_uptime()
-        CPU = getCPUusage()
-        memory = getMemory()
-        freeDisk = getDiskSpace()
-        CPUHealth = True
-        memoryHealth = True
-        diskHealth = True
-        if CPU > 50:
-            print("\a[WARNING] sustained higher CPU usage\nCPU has averaged " + str(CPU) + "% for 5 minutes")
-            CPUHealth = False
-        if memory < 10:
-            print("\a[WARNING] RAM usage is at " + str(100-memory) + "%")
-            memoryHealth = False
-        if freeDisk < 10:
-            print("\a[WARNING] Disk almost full, only " + str(freeDisk) + "% remaning")
-            diskHealth = False
-        if CPUHealth and memoryHealth and diskHealth:
-            print("system is healthy")
-        time.sleep(5)
+        print("System Health Check, please select option below")
+        print("1. continuous monitoring")
+        print("2. quick monitor (takes 1 min)")
+        print("3. snapshot (view resources at this exact moment)")
+        print("0. exit")
+        choice = inputInt("option: ")
+        print(choice)
 
 
 
